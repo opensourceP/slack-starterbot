a28bb31b42bf6cc5f2d9331b1669269ea384d069
Ji Ho Bok <treehouse31@naver.com>
2017-12-04 23:53:50 +0900

    bokbot 기온에 따른 옷 추천 기능 추가

--- starterbot.py
+++ starterbot.py
@@ -19,9 +19,26 @@ STARTER = "starter"
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN')) 
 
 def bokbot(command,channel):
+     
+     temp=input("오늘의 기온을 입력해주세요(섭씨온도(℃): ")
+     if temp >= 27:
+          response = "오늘은 나시티, 반팔 티셔츠, 반바지, 민소매 옷을 입기에 적절한 날이네요! :)"
+     elif temp >= 23 and temp < 27:
+          response = "오늘은 반팔, 얇은 셔츠, 얇은 긴팔, 반바지, 면바지를 입기에 적절한 날이네요~ :)"
+     elif temp >= 20 and temp < 23:
+          response = "오늘은 긴팔티, 가디건, 얇은 후드티, 면바지, 슬랙스, 스키니 등을 입기에 적절한 날이네요!"
+     elif temp >= 17 and temp < 20:
+          response = "얇은 니트, 가디건, 후드티나 맨투맨과 청바지, 면바지, 슬랙스 등을 매치해서 입으면 좋을 날이에요 ^ㅠ^"
+     elif temp >= 12 and temp < 17:
+          response = "자켓, 셔츠 가디건이나 야상 같은 겉옷을 챙기시면 좋을 날이에요!"
+     elif temp >= 6 and temp < 9:
+          response = "코트나 가죽자켓 같은 겉옷에 맨투맨, 니트, 후드티 등을 속에 입으면 적당할 날이에요!"
+     elif temp < 6:
+          response = "패딩 같은 두꺼운 겉옷에 장갑, 목도리도 착용해야 될 날이에요! 읏추읏추~"
      slack_client.api_call("chat.postMessage", channel=channel, 
                           text=response, as_user=True)
-
+          
+     
 
 def chobot(command,channel):
      slack_client.api_call("chat.postMessage", channel=channel, 
